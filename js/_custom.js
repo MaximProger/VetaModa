217feedc1fa99e78b651c9efd4aadfd72799d475
Maxim Koshman <maksimkoshman090@gmail.com>
2020-01-21 21:18:10 +0300

    add some pages

--- js/_custom.js
+++ js/_custom.js
@@ -1,3 +1,11 @@
 $(function() {
-
+    // $('#catalogRubaski').on('click', function(event) {
+    //     event.preventDefault();
+    //     location.href = "index.html";
+    //     $('.catalog__tab').removeClass('active');
+    //     $('.catalog__tab').removeClass('show');
+    //     $('#rubashki').addClass('active');
+    //     $('#rubashki').addClass('show');
+    //     $('#2').addClass('active');
+    // })
 });
