df2e278e23c15346d76e071f7adf1eefe0c0ae6b
Maxim Koshman <maksimkoshman090@gmail.com>
2020-01-22 10:50:10 +0300

    add mobile state for main page

--- js/_custom.js
+++ js/_custom.js
@@ -1,11 +1,63 @@
 $(function() {
-    // $('#catalogRubaski').on('click', function(event) {
-    //     event.preventDefault();
-    //     location.href = "index.html";
-    //     $('.catalog__tab').removeClass('active');
-    //     $('.catalog__tab').removeClass('show');
-    //     $('#rubashki').addClass('active');
-    //     $('#rubashki').addClass('show');
-    //     $('#2').addClass('active');
-    // })
+
+    $('#catalog-bluzki').on('click', function(event) {
+        event.preventDefault();
+        location.href = "bluzki.html";
+        
+    })
+    $('#catalog-rubashki').on('click', function(event) {
+        event.preventDefault();
+        location.href = "rubashki.html";
+        
+    })
+    $('#catalog-tuniki').on('click', function(event) {
+        event.preventDefault();
+        location.href = "tuniki.html";
+        
+    })
+    $('#catalog-dzinsi').on('click', function(event) {
+        event.preventDefault();
+        location.href = "dzinsi.html";
+        
+    })
+    $('#catalog-bruki').on('click', function(event) {
+        event.preventDefault();
+        location.href = "bruki.html";
+        
+    })
+    $('#catalog-platya').on('click', function(event) {
+        event.preventDefault();
+        location.href = "platya.html";
+        
+    })
+    $('#catalog-ubki').on('click', function(event) {
+        event.preventDefault();
+        location.href = "ubki.html";
+        
+    })
+    $('#catalog-verh_odez').on('click', function(event) {
+        event.preventDefault();
+        location.href = "verh_odez.html";
+        
+    })
+    $('#catalog-kostumi').on('click', function(event) {
+        event.preventDefault();
+        location.href = "kostumi.html";
+        
+    })
+    $('#catalog-dzemper').on('click', function(event) {
+        event.preventDefault();
+        location.href = "dzemper.html";
+        
+    })
+    $('#catalog-belyo').on('click', function(event) {
+        event.preventDefault();
+        location.href = "belyo.html";
+        
+    })
+    $('#catalog-sumki').on('click', function(event) {
+        event.preventDefault();
+        location.href = "sumki.html";
+        
+    })
 });
